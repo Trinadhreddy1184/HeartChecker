620c9ed5fe5ede4756789d203fdb744091a32c6a
trinadhreddy <trinadhreddysee@gmail.com>
2023-11-14 13:03:50 -0500

    some ml code modification but not anticipated2

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 import base64
 from io import BytesIO
-import shap
-import plotly as plt
+# import shap
+# import plotly as plt
 
 from ml_code.ml_manager import MLManager
 from ml_code.ml_visualizer import MLVisualizer
@@ -19,19 +19,19 @@ ml_instance = MLManager("/Users/trinadhreddy/Downloads/heart.csv")
 def index():
     global ml_instance  # Define ml_instance in the scope of the function
 
-    if request.method == 'POST':
-        # Process user input and interact with the MLManager class
-        user_input = {
-            'age': [float(request.form['age'])],
-            'sex': [int(request.form['sex'])],
-            # ... Include other features
-        }
-        predictions = ml_instance.predict(user_input)
-        probability = ml_instance.predict_proba(user_input)
-        deviations = ml_instance.calculate_attribute_deviations(
-            pd.DataFrame(user_input, columns=ml_instance.feature_names))
-        return render_template('result.html', predictions=predictions, probability=probability,
-                               deviations=deviations)
+    # if request.method == 'POST':
+    #     # Process user input and interact with the MLManager class
+    #     user_input = {
+    #         'age': [float(request.form['age'])],
+    #         'sex': [int(request.form['sex'])],
+    #         # ... Include other features
+    #     }
+    #     predictions = ml_instance.predict(user_input)
+    #     probability = ml_instance.predict_proba(user_input)
+    #     deviations = ml_instance.calculate_attribute_deviations(
+    #         pd.DataFrame(user_input, columns=ml_instance.feature_names))
+    #     return render_template('result.html', predictions=predictions, probability=probability,
+    #                            deviations=deviations)
     return render_template('index.html')
 @app.route('/checkyourheart')
 def checkyourheart():
@@ -51,14 +51,14 @@ def dashboard():
     probability = ml_instance.predict_proba(X_test)
 
     # Visualize results
-    img = BytesIO()
-    explainer = shap.TreeExplainer(ml_instance.model)
-    shap_values = explainer.shap_values(X_test)
-    shap.summary_plot(shap_values[1], X_test)
-    plt.savefig(img, format='png')
-    img_str = "data:image/png;base64," + base64.b64encode(img.getvalue()).decode()
-
-    return render_template('dashboard.html', img_str=img_str)
+    # # img = BytesIO()
+    # # explainer = shap.TreeExplainer(ml_instance.model)
+    # # shap_values = explainer.shap_values(X_test)
+    # # shap.summary_plot(shap_values[1], X_test)
+    # # plt.savefig(img, format='png')
+    # img_str = "data:image/png;base64," + base64.b64encode(img.getvalue()).decode()
+    #
+    # return render_template('dashboard.html', img_str=img_str)
 
 if __name__ == '__main__':
     app.run(debug=True)
