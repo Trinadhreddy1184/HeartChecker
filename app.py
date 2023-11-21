4235aad472f66b3b3ce7b26ba77bbd802351b27d
trinadhreddy <trinadhreddysee@gmail.com>
2023-11-21 01:16:47 -0500

    some ml code modification but not anticipated5

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # File: app.py
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import base64
@@ -59,6 +59,38 @@ def dashboard():
     # img_str = "data:image/png;base64," + base64.b64encode(img.getvalue()).decode()
     #
     # return render_template('dashboard.html', img_str=img_str)
+@app.route('/result', methods=['POST'])
+def result():
+    if request.method == 'POST':
+        # Get user input from the form
+        user_input = {
+            'age': float(request.form['age']),
+            'sex': int(request.form['sex']),
+            'cp': int(request.form['cp']),
+            'trestbps': int(request.form['trestbps']),
+            'chol': int(request.form['chol']),
+            'fbs': int(request.form['fbs']),
+            'restecg': int(request.form['restecg']),
+            'thalach': int(request.form['thalach']),
+            'exang': int(request.form['exang']),
+            'oldpeak': float(request.form['oldpeak']),
+            'slope': int(request.form['slope']),
+            'num_major_vessels': int(request.form['num_major_vessels']),
+            'thal': int(request.form['thal'])
+        }
+
+
+        # Render the result template with user input and predictions
+        return render_template('result.html', age=user_input['age'], sex=user_input['sex'],
+                                cp=user_input['cp'], trestbps=user_input['trestbps'],
+                                chol=user_input['chol'], fbs=user_input['fbs'],
+                                restecg=user_input['restecg'], thalach=user_input['thalach'],
+                                exang=user_input['exang'], oldpeak=user_input['oldpeak'],
+                                slope=user_input['slope'], num_major_vessels=user_input['num_major_vessels'],
+                                thal=user_input['thal'])
+
+    return "Method not allowed"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
